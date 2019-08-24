128f85b2a0d2d7b01211ad0d211a3b11529f3920
danielbyun <dbyun92@gmail.com>
2019-08-25 00:10:57 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  /*
+  git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/danielbyun/burger-builder.git
+git push -u origin master
+  */
   return (
     <div className="App">
       <header className="App-header">
