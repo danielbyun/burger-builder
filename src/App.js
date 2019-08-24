9a427c40dcec48be128518da4c52ae09d3728a76
danielbyun <dbyun92@gmail.com>
2019-08-25 00:36:11 +0900

    Starting Implementation of The Burger Builder Container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 function App() {
   return (
     <div>
       <Layout>
-        <h1>This is a test</h1>
-        <p>so everything in here prints in the Layout children component</p>
-        <p>Test</p>
+        <BurgerBuilder />
       </Layout>
     </div>
   );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+import "./Layout.css";
 
 const Layout = props => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className="Content">{props.children}</main>
   </Aux>
 );
 
