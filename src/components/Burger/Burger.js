6863fd121e70e4b19cd2993a78456b8370d9a04e
danielbyun <dbyun92@gmail.com>
2019-09-01 09:37:00 +0900

    dynamically generating burger ingredients (implementing basic burger builder feature)

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,35 @@
+import React from "react";
+import classes from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = props => {
+  // converting objects to arrays
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredientKey => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        return (
+          <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+        );
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  // if null
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
+  console.log(transformedIngredients);
+
+  return (
+    <div className="Burger">
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
