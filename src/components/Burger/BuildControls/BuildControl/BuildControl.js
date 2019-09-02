df3b7b7f24ddfa8460e9aed28b7b035b507c27ba
danielbyun <dbyun92@gmail.com>
2019-09-02 22:47:24 +0900

    display the price

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,16 +2,12 @@ import React from "react";
 import "./BuildControl.css";
 
 const buildControl = props => (
-  <div className={"BuildControl"}>
-    <div className={"Label"}>{props.label}</div>
-    <button
-      className={"Less"}
-      onClick={props.removed}
-      disabled={props.disabled}
-    >
+  <div className="BuildControl">
+    <div className="Label">{props.label}</div>
+    <button className="Less" onClick={props.removed} disabled={props.disabled}>
       Less
     </button>
-    <button className={"More"} onClick={props.added}>
+    <button className="More" onClick={props.added}>
       More
     </button>
   </div>
