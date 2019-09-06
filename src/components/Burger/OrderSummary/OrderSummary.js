6ceda7ace0ddfc4bc105741f7b5ea0e089acc5f6
danielbyun <dbyun92@gmail.com>
2019-09-07 01:03:55 +0900

    create order / cancel buttons and style

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,6 +17,8 @@ const OrderSummary = props => {
       <p>Delicious burger with the following ingredients: </p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to checkout</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </Aux>
   );
 };
