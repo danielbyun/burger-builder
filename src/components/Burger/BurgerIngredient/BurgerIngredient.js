9066430858a5c1b7360eaab374c0b64238c41637
danielbyun <dbyun92@gmail.com>
2019-09-07 16:28:40 +0900

    side drawer feature implemented

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import classes from "./BurgerIngredient.css";
 
 const BurgerIngredient = props => {
   let ingredient = null;
