b4af4e713bf615d2734da60a07e6f1739c085ef9
danielbyun <dbyun92@gmail.com>
2019-09-12 14:44:37 +0900

    adding custom form validation bug: checks one condition after one another so it might pass even if it should not pass

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,11 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Your Name"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
       street: {
         elementType: "input",
@@ -22,7 +26,11 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Street"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
       state: {
         elementType: "input",
@@ -30,7 +38,11 @@ class ContactData extends Component {
           type: "text",
           placeholder: "State"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
       zipCode: {
         elementType: "input",
@@ -38,7 +50,13 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Zipcode"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false
       },
       country: {
         elementType: "input",
@@ -46,7 +64,11 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Country"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
       email: {
         elementType: "input",
@@ -54,7 +76,11 @@ class ContactData extends Component {
           type: "email",
           placeholder: "Your Email"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
       deliveryMethod: {
         elementType: "select",
@@ -70,12 +96,6 @@ class ContactData extends Component {
         },
         value: ""
       }
-    },
-    name: "",
-    email: "",
-    address: {
-      street: "",
-      postalCode: ""
     },
     loading: false
   };
@@ -124,10 +144,25 @@ class ContactData extends Component {
       });
   };
 
-  inputChangedHandler = (event, inputIdentifier) => {
-    console.log(event.target);
-    console.log(inputIdentifier);
+  checkValidity(value, rules) {
+    let isValid = false;
 
+    if (rules.required) {
+      // update true or false depending on the check value if it is not equal to empty string
+      isValid = value.trim() !== " ";
+    }
+
+    if (rules.minLength) {
+      // expect value to be 1, 2, 3...so on
+      isValid = value.length >= rules.minLength;
+    } else if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength;
+    }
+
+    return isValid;
+  }
+
+  inputChangedHandler = (event, inputIdentifier) => {
     // cloning deeply
     const updatedOrderForm = {
       ...this.state.orderForm
@@ -136,7 +171,13 @@ class ContactData extends Component {
       ...updatedOrderForm[inputIdentifier]
     };
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
     updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+    console.log(updatedFormElement);
 
     this.setState({
       orderForm: updatedOrderForm
