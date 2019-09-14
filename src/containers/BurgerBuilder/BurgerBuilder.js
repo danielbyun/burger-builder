2d7b79816a9dc4390c787b06fbad024f453321c7
danielbyun <dbyun92@gmail.com>
2019-09-14 11:40:59 +0900

    finishing the reducer for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,16 +32,16 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
-    axios
-      .get("https://react-burger-builder-2a041.firebaseio.com/ingredients.json")
-      .then(response => {
-        this.setState({
-          ingredients: response.data
-        });
-      })
-      .catch(error => {
-        this.setState({ error: true });
-      });
+    // axios
+    //   .get("https://react-burger-builder-2a041.firebaseio.com/ingredients.json")
+    //   .then(response => {
+    //     this.setState({
+    //       ingredients: response.data
+    //     });
+    //   })
+    //   .catch(error => {
+    //     this.setState({ error: true });
+    //   });
   }
 
   updatePurchaseState(ingredients) {
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,39 @@
 import * as actionTypes from "./actions";
 
 const initialState = {
-  ingredients: null,
+  ingredients: {
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0
+  },
   totalPrice: 4
 };
 
-const reducer = (state = initialState, action) => {};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      return {
+        // copy the old state and create deep clone objects
+        ...state,
+        // new object
+        ingredients: {
+          ...state.ingredients,
+          // can change the property name
+          [action.ingedientName]: state.ingredients[action.ingredientName] + 1
+        }
+      };
+    case actionTypes.REMOVE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        }
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
