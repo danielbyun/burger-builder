4ccba6be0ecaf383b946f29d8a3df1df2bd7186d
danielbyun <dbyun92@gmail.com>
2019-09-14 12:12:07 +0900

    redux & UI state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,6 @@ class BurgerBuilder extends Component {
   // }
 
   state = {
-    purchasable: false,
     // local UI state (no redux needed [not required])
     purchasing: false,
     loading: false,
@@ -46,9 +45,10 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({
-      purchasable: sum > 0
-    });
+    // this.setState({
+    //   purchasable: sum > 0
+    // });
+    return sum > 0;
   }
 
   // addIngredientHandler = type => {
@@ -139,7 +139,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
             price={this.props.price}
           />
