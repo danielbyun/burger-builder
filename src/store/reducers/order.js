7e8a79e542e797b5dcbd621f4a43e69dd164a9b0
danielbyun <dbyun92@gmail.com>
2019-09-22 14:52:36 +0900

    working on order actions

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -7,6 +7,11 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case actiontypes.PURCHASE_BURGER_START:
+      return {
+        ...state,
+        loading: true
+      };
     case actiontypes.PURCHASE_BURGER_SUCCESS:
       const newOrder = {
         ...action.orderData,
